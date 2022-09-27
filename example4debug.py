c750f8142ef904ce01a4cadce2d3c57fc705415d
BingzhaoZhu <bingzhaozhu@gmail.com>
2022-09-27 20:38:02 +0000

    Added Pretrain

--- example4debug.py
+++ example4debug.py
@@ -91,14 +91,14 @@ def train(args):
 
     metric = "accuracy"
     hyperparameters = {} if args.mode == "FT_Transformer" else get_hyperparameter_config('default')
-    # hyperparameters['FT_TRANSFORMER'] = {"env.num_gpus": args.num_gpus,
-    #                                      "env.num_workers": args.num_workers,
-    #                                      "env.per_gpu_batch_size": 64,
-    #                                      "pretrainer": True,
-    #                                      }
-    hyperparameters['CAT'] = {"pretrainer": True,
-                              "random_seed": 10,
-                              "ag.early_stop": 3}
+    hyperparameters['FT_TRANSFORMER'] = {"env.num_gpus": args.num_gpus,
+                                         "env.num_workers": args.num_workers,
+                                         "env.per_gpu_batch_size": 64,
+                                         "pretrainer": True,
+                                         }
+    # hyperparameters['CAT'] = {"pretrainer": True,
+    #                           "random_seed": 10,
+    #                           "ag.early_stop": 3}
 
     predictor = TabularPredictor(label=label,
                                  eval_metric=metric)
@@ -106,7 +106,7 @@ def train(args):
         train_data=df_train,
         hyperparameters=hyperparameters,
         holdout_frac=0.125,
-        time_limit=3000,)
+        time_limit=300,)
     predictor.predict(df_test)
     leaderboard = predictor.leaderboard(df_test)
     leaderboard.to_csv("./leaderboard.csv")
