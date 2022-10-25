3570fc45dc548c58e49e177641a6748f9d730719
BingzhaoZhu <bingzhaozhu@gmail.com>
2022-10-25 04:39:31 +0000

    always init bias with zeros and free tokens from weight decay

--- multimodal/src/autogluon/multimodal/optimization/utils.py
+++ multimodal/src/autogluon/multimodal/optimization/utils.py
@@ -402,7 +402,7 @@ def apply_single_lr(
         },
         {
             "params": [p if return_params else n for n, p in model.named_parameters() if (n in decay_param_names and "row" in n)],
-            "weight_decay": 0.01,
+            "weight_decay": 1,
             "lr": lr,
         },
         {
